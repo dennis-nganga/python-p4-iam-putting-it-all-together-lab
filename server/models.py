8c19fe2a07ca9af000e936019a37687dbca675b8
dennis-nganga <126565131+dennis-nganga@users.noreply.github.com>
2023-07-08 23:22:13 +0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -1,14 +1,56 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('password_hash is not readable')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    @hybrid_property
+    def serialized(self):
+        return self.to_dict()
+
+    def validate(self):
+        errors = {}
+        if not self.title:
+            errors['title'] = 'Title is required.'
+        if not self.instructions or len(self.instructions) < 50:
+            errors['instructions'] = 'Instructions must be at least 50 characters long.'
+        return errors
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'title': self.title,
+            'instructions': self.instructions,
+            'minutes_to_complete': self.minutes_to_complete
+        }
+
+db.create_all()
